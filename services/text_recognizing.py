c5d88cf074fe093aeef3104bb911017ea5380948
JewgeniKr <kryukov.develop@gmail.com>
2025-12-08 15:42:09 +0700

    добавлен модуль с функционалом распознавания текста

---
+++ services/text_recognizing.py
@@ -0,0 +1,59 @@
+import os
+from pathlib import Path
+import configparser
+from PIL import Image
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+
+class TextRecognition:
+    def __init__(self, model_folder, image_folder, rec_fields_numbers):
+        self.model_folder = model_folder
+        self.image_folder = image_folder
+        self.rec_fields_numbers = rec_fields_numbers
+
+    def get_text(self):
+        rec_text = {}
+
+        # загрузка модели и процессора
+        processor = TrOCRProcessor.from_pretrained(self.model_folder)
+        model = VisionEncoderDecoderModel.from_pretrained(self.model_folder)
+
+        folder = Path(self.image_folder).iterdir()
+        for file in folder:
+            file_name = os.path.basename(file)
+            field_number = file_name[:file_name.find('_')]
+
+            if field_number in self.rec_fields_numbers:
+                field_name = self.get_field_name(file_name)
+                image = Image.open(file).convert("RGB")
+
+                # генерация предсказаний
+                pixel_values = processor(images=image, return_tensors="pt").pixel_values
+                generated_ids = model.generate(pixel_values)
+                predicted_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+                rec_text[field_name] = predicted_text
+
+        return rec_text
+
+    @staticmethod
+    def get_field_name(file_name):
+        first_symbol = file_name.find('_') + 1
+        last_symbol = file_name.find('_conf')
+
+        field_name = file_name[first_symbol:last_symbol]
+
+        return field_name
+
+if __name__ == '__main__':
+    ROOT_PATH = Path(__file__).resolve().parents[1]
+
+    config = configparser.ConfigParser()
+    config.read(f'{ROOT_PATH}/settings.ini')
+    rec_fields_numbers = config['RecFieldsNumbers']['numbers']
+
+    model_path = 'C:/_waybill_recognition/prod_1/models/model_from_pretrained_e250'
+    img_path = 'C:/_waybill_recognition/prod_1/temp/recognitions/172/5b23ce7f-61e8-47d5-8ffc-081d75664b1d_1/text_boxes'
+    tr = TextRecognition(model_path, img_path, rec_fields_numbers)
+    text_dict = tr.get_text()
+
+    print(text_dict)
