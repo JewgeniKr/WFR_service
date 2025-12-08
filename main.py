b298ded5c275b7dd0f79c67757674423e5abb0c1
Joe <kryukov.develop@gmail.com>
2025-12-08 21:31:20 +0700

    добавлен роут для загрузки файла на сервер

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import os
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'temp/pdf'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 
 @app.route('/')
 def root():
     return render_template('index.html', title='Главная')
+
+@app.route('/recognize', methods=['POST'])
+def recognize():
+    if 'uploaded_file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['uploaded_file']
+
+    # Проверяем, выбрал ли пользователь файл
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file:
+        # Сохраняем файл
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        return jsonify({
+            'message': 'File uploaded successfully',
+            'filename': filename,
+            'path': file_path
+        }), 200
 
 
 if __name__ == '__main__':
